18ec867f901dab56cfb2febb06d72857a048a5b7
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-02-23 13:48:40 +0100

    “branch-name”

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <h1>Roots</h1>
+    </div>
+  )
+}
+
+export default App
