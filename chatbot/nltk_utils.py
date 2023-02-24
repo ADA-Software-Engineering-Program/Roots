bd0b00da3c8ac296a19fcc40834b0e0a999aa274
Marzz <mariamadesina29@gmail.com>
2023-02-24 12:26:23 +0100

    perf: :zap: updated intents.json, nltk_utils.py, and train.py
    Added tokenization and stemming functions for the intents in the JSON file which contains tags, patterns, and responses.

--- chatbot/nltk_utils.py
+++ chatbot/nltk_utils.py
@@ -1,2 +1,17 @@
 import numpy as np
 import nltk
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence,all_words):
+    pass
+
+words = ["organize", "orGanizes", "organizing", "Organization"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
--- chatbot/train.py
+++ chatbot/train.py
@@ -4,3 +4,8 @@ import json
 
 import torch
 import torch.nn as nn
+
+with open('chatbot\intents.json', 'r') as f:
+    intents = json.load(f)
+
+print(intents)
