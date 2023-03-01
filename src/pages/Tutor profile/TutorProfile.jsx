e645dea59fa31bdf115ade9dc534d29ef7a6ae4d
Ifechukwu <ifechiglory@gmail.com>
2023-03-01 13:02:45 +0100

    fix pages: tutors

--- src/pages/Tutor profile/TutorProfile.jsx
+++ src/pages/Tutor profile/TutorProfile.jsx
@@ -5,6 +5,9 @@ function Tutor() {
     return(
         <section className='tutor container-fluid p-3'>
             <div className='row'>
+                <img src='https://us.lifelovingfoods.com/wp-content/uploads/2020/12/under-construction-gif-11.gif' alt='construct' className='img-responsive'></img>
+            </div>
+            {/* <div className='row'>
                 <div className='col-12 tutor_heading'>
                     <h2>Meet your Tutors!</h2>
                 </div>
@@ -16,7 +19,7 @@ function Tutor() {
                     <img alt='Profile Image' src='https://images.unsplash.com/photo-1551727981-bfe3e86eaa00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhlYWRzaG90JTIwb2YlMjBibGFjayUyMHRlYWNoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'  className='img-responsive profile_image'></img>
                 </div>
                 <div className='col-6'></div>
-            </div>
+            </div> */}
         </section>
     )
 }
