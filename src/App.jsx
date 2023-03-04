44bf0b7d139dd6960c8b98deeee8f0c939909b51
Ifechukwu <ifechiglory@gmail.com>
2023-03-04 07:14:33 +0100

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/" element={<UserDashboard />} />
           <Route path="/tutor" element={<Tutor />} />
           <Route path="/community" element={<Community />} />
-          <Route path='/courses' element={<Course/>}/>
+          <Route path='/course' element={<Course/>}/>
           <Route path='/profile' element={<UserProfile />} />
           <Route path='/notification' element={<UserNotification />} />
           <Route path='/security' element={<UserSecurity />} />
